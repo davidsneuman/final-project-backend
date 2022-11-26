fddc6d4fbf8105a0bec180a9d6c96bc5a84e8613
David Neuman <davidsneuman@gmail.com>
2022-11-26 13:45:35 -0500

    Update student model

--- database/models/Student.js
+++ database/models/Student.js
@@ -15,7 +15,21 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
-  }
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL,
+  },
 });
 
 // Export the student model
